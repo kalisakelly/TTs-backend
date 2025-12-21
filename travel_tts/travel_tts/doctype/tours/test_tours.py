49b50107ab1dcf09cb7d36ade12c4750f7892619
kalisakelly <kalisakelly2001@gmail.com>
2025-12-21 18:31:25 +0200

    initial commit

---
+++ travel_tts/travel_tts/doctype/tours/test_tours.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, kalisakelly and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTours(FrappeTestCase):
+	pass
---
+++ travel_tts/travel_tts/doctype/tours/tours.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, kalisakelly and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tours", {
+// 	refresh(frm) {
+
+// 	},
+// });
