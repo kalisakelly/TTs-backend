ae18083e9420b851eddd04a7d74aad5c33a19775
kalisakelly <kalisakelly2001@gmail.com>
2026-04-13 22:11:55 +0200

    modifying and add components

---
+++ travel_tts/travel_tts/doctype/landing_page_image/landing_page_image.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, kalisakelly and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Landing page image", {
+// 	refresh(frm) {
+
+// 	},
+// });
